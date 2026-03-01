418f7d1e740457139e8e4820b6264d5262b1ce78
Maimoon-Madrasa-Web <maimoonmadrasa@gmail.com>
2026-03-01 12:19:55 +0530

    refactor: sort student exam results based on raw score instead of calculated percentages

--- temp.js
+++
@@ -1,9 +0,0 @@
-
-    if (localStorage.getItem('madrasa_theme') === 'light') {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-    if (localStorage.getItem('madrasa_uid')) {
-      document.body.classList.add('auto-logging-in');
-      document.getElementById('global-loader').style.display = 'flex';
-    }
-  
